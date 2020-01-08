c0734cbee018f19fb745beb9577e5a6e8ede45ba
Mohsin Ali <mohsinit49@gmail.com>
2020-01-08 21:14:22 +0500

    14 to 19

---
+++ script.js
@@ -0,0 +1,62 @@
+// Arrays
+var arr = [3, 4, 5];
+// var popedArr = arr.pop();
+// alert("Popped Arr :" + popedArr)
+// alert("Original Arr :" + arr)
+
+// arr.unshift(0, 1, 2);
+// // alert("pushedArr Arr :" + pushedArr)
+// alert("Original Arr :" + arr)
+
+// var pets = ["dog", "cat", "fly", "bug", "ox", "fly2", "bug2", "ox2"];
+// // var splicedPets = pets.splice(2, 2, "pig", "duck", "emu");
+// // pets.splice(0, 3, "pig", "duck", "emu");
+// // alert(pets)
+// // pets.slice(0, 2) not recommended
+// var sliced = pets.slice(2, 7) //recommended
+// alert(sliced)
+
+
+// Loops
+
+// for(var i = 1; i <=10; i++){
+//     console.log(i)
+// }
+
+// var cleanestCities = ['ISB', 'PSW', 'JRW', 'FSD', 'MARDAN']
+// var cityToCheck = prompt('Please enter city name');
+// var matchFound = "no"; //Flags
+// for (var i = 0; i <= cleanestCities.length; i++) {
+//     if (cityToCheck === cleanestCities[i]) {
+//         matchFound = "yes";
+//     }
+// }
+// if (matchFound === "yes") {
+//     alert("It's one of the cleanest cities");
+// }
+// if (matchFound === "no") {
+//     alert("It's not on the list");
+// }
+
+
+// Boleans
+// var cleanestCities = ['ISB', 'PSW', 'JRW', 'FSD', 'MARDAN']
+// var cityToCheck = prompt('Please enter city name');
+// var matchFound = false;
+// for (var i = 0; i <= 4; i++) {
+//     if (cityToCheck === cleanestCities[i]) {
+//         matchFound = true;
+//         break;
+//     }
+//     console.log(cleanestCities[i])
+// }
+// if (matchFound) {
+//     alert("It's one of the cleanest cities");
+// } 
+// if (!matchFound) {
+//     alert("It's not on the list");
+
+// }
+// else {
+//     alert("It's not on the list");
+// }
